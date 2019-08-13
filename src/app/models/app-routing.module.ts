a232ab622c20873878439710f6440b9556a7a785
Liam Mccabe <gordon.m@live.co.za>
2019-08-13 19:42:08 +0200

    App routing module with rolegaurd

--- src/app/models/app-routing.module.ts
+++ src/app/models/app-routing.module.ts
@@ -1,20 +1,15 @@
-
-
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RegistrationComponent } from '../pages/registration/registration.component';
 import { LoginComponent } from '../pages/login/login.component';
 import {MyRFCSComponent} from '../pages/my-rfcs/my-rfcs.component';
 import {CreateRfcsComponent} from '../pages/create-rfcs/create-rfcs.component';
-
 import {HomeComponent} from '../pages/home/home.component';
 import {CurrentRFCSComponent} from '../pages/current-rfcs/current-rfcs.component';
 import {RFCsignoffComponent} from '../pages/rfcsignoff/rfcsignoff.component';
 import {Phase1Component} from '../pages/phase1/phase1.component';
 import {RouteGuard} from '../guards/route.guard';
 import {AuthGuard} from '../guards/auth.guard';
-
-
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
