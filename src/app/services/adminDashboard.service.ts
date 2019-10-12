7b29f742c5bbef1346d67d1f2c8141d03155c47a
unknown <unknown@example.com>
2019-10-12 13:59:38 +0200

    Admin add and remove functionality added

--- src/app/services/adminDashboard.service.ts
+++ src/app/services/adminDashboard.service.ts
@@ -45,6 +45,91 @@ export class AdminDashboardService{
         return this.http.post('http://localhost/ATNSCCR_PHP/backend/api2/removeFiles.php', obj, httpOptions);
     }
 
+    editUserRole(user, role, operation){
+        if(operation === 'add'){
+            const obj = {insertSelection: 'userRole', userID: user, roleID: role};
+            return this.http.post('http://localhost/ATNSCCR_PHP/backend/api2/insertData.php', obj, httpOptions);
+        } else {
+            const obj = {deleteSelection: 'userRole', userID: user, roleID: role};
+            return this.http.post('http://localhost/ATNSCCR_PHP/backend/api2/removeData.php', obj, httpOptions);
+        }
+    }
+
+    assignEOS(user, eos, operation){
+        if(operation === 'add'){
+            const obj = {insertSelection: 'eosAssign', userID: user, eosID: eos};
+            return this.http.post('http://localhost/ATNSCCR_PHP/backend/api2/insertData.php', obj, httpOptions);
+        } else {
+            const obj = {deleteSelection: 'eosAssign', userID: user, eosID: eos};
+            return this.http.post('http://localhost/ATNSCCR_PHP/backend/api2/removeData.php', obj, httpOptions);
+        }
+    }
+
+    assignSite(user, site, operation){
+        if(operation === 'add'){
+            const obj = {insertSelection: 'siteAssign', userID: user, siteID: site};
+            return this.http.post('http://localhost/ATNSCCR_PHP/backend/api2/insertData.php', obj, httpOptions);
+        } else {
+            const obj = {deleteSelection: 'siteAssign', userID: user, siteID: site};
+            return this.http.post('http://localhost/ATNSCCR_PHP/backend/api2/removeData.php', obj, httpOptions);
+        }
+    }
+
+    getSiteUser(){
+        const obj = {dataSelection: 'siteAssign'};
+        return this.http.post('http://localhost/ATNSCCR_PHP/backend/api2/pageData.php', obj, httpOptions);
+    }
+
+    getEOSUser(){
+        const obj = {dataSelection: 'eosAssign'};
+        return this.http.post('http://localhost/ATNSCCR_PHP/backend/api2/pageData.php', obj, httpOptions);
+    }
+
+    insertTitle(type, description){
+        const obj = {insertSelection: 'title', type: type, description: description};
+        return this.http.post('http://localhost/ATNSCCR_PHP/backend/api2/insertData.php', obj , httpOptions);
+    }
+
+    deleteTitle(id){
+        const obj = {deleteSelection: 'title', ID: id};
+        return this.http.post('http://localhost/ATNSCCR_PHP/backend/api2/removeData.php', obj, httpOptions);
+    }
+
+    getTitle(){
+        const obj = {dataSelection: 'title'};
+        return this.http.post('http://localhost/ATNSCCR_PHP/backend/api2/pageData.php', obj, httpOptions);
+    }
+
+    insertManager(job, site, first, last, phone, email){
+        const obj = {insertSelection: 'manager', job: job, site: site, first: first, last: last, phone: phone, email: email};
+        return this.http.post('http://localhost/ATNSCCR_PHP/backend/api2/insertData.php', obj , httpOptions);
+    }
+
+    deleteManager(id){
+        const obj = {deleteSelection: 'manager', ID: id};
+        return this.http.post('http://localhost/ATNSCCR_PHP/backend/api2/removeData.php', obj, httpOptions);
+    }
+
+    getManager(){
+        const obj = {dataSelection: 'manager'};
+        return this.http.post('http://localhost/ATNSCCR_PHP/backend/api2/pageData.php', obj, httpOptions);
+    }
+
+    insertEOS(type, description){
+        const obj = {insertSelection: 'eos', type: type, description: description};
+        return this.http.post('http://localhost/ATNSCCR_PHP/backend/api2/insertData.php', obj , httpOptions);
+    }
+
+    deleteEOS(id){
+        const obj = {deleteSelection: 'eos', ID: id};
+        return this.http.post('http://localhost/ATNSCCR_PHP/backend/api2/removeData.php', obj, httpOptions);
+    }
+
+    getEOS(){
+        const obj = {dataSelection: 'eos'};
+        return this.http.post('http://localhost/ATNSCCR_PHP/backend/api2/pageData.php', obj, httpOptions);
+    }
+
     getPageData() {
         const obj = {
             dataSelection: 'allCCR'
