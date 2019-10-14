8d4c876ca0f31e942b2af77c219c1084253cfd6b
Liam <gordon.m@live.co.za>
2019-10-14 19:42:34 +0200

    Update to card display data In my-rfcs

--- src/app/pages/my-rfcs/my-rfcs.component.ts
+++ src/app/pages/my-rfcs/my-rfcs.component.ts
@@ -7,6 +7,9 @@ import decode from 'jwt-decode';
 import {CurrentCcrsService} from '../../services/current-ccrs.service';
 import {Phase2Service} from '../../services/phase2.service';
 import {Phase3Service} from '../../services/phase3.service';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {Ccrphase1ContentComponent} from '../current-rfcs/ccrphase1-content/ccrphase1-content.component';
+import {Ccrphase2ContentComponent} from '../current-rfcs/ccrphase2-content/ccrphase2-content.component';
 
 @Component({
   selector: 'app-my-rfcs',
@@ -17,10 +20,19 @@ export class MyRFCSComponent implements OnInit {
 
   private myCCRs ;
 
-  constructor(private myRfcsService: MyRfcsService){ }
+  constructor(private myRfcsService: MyRfcsService , private modalService: NgbModal) { }
 
   private displayPhase2: boolean;
   private displayPhase3: boolean;
+
+    viewCcrPhase1(phase1) {
+        const modalRef = this.modalService.open(Ccrphase1ContentComponent , { centered: true });
+        modalRef.componentInstance.phase1 = phase1;
+    }
+    viewCcrPhase2(phase2) {
+        const modalRef = this.modalService.open(Ccrphase2ContentComponent , { centered: true , size: 'lg'});
+        modalRef.componentInstance.phase2 = phase2;
+    }
 
   ngOnInit() {
     this.myRfcsService.getMyCCRs().subscribe( (data: Data) => {
